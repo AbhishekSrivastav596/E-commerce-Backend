4134b1d180f58291d315c27787bd1ef467a00a7f
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-01-21 22:05:41 +0530

    fix port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 require('./mongooseConnection');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
